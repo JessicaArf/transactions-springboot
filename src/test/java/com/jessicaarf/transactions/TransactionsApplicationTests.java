fec3c6609e05752de063b2c9c3c4f47a38533ef3
Jessica Arf <jessicalealarf8@gmail.com>
2023-11-14 23:53:14 -0300

    project created

---
+++ src/test/java/com/jessicaarf/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessicaarf.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
